e86554110c88b4f7607688b4f5e74b58956a7e4f
Norman Th. Glaves <ntglaves@falabella.cl>
2019-10-17 16:16:42 -0300

    add logic - is mutant

---
+++ src/logic/is_mutant.js
@@ -0,0 +1,19 @@
+module.exports = (ln) => {
+    let flag = false;
+    let safe = 0;
+    let char = '';
+
+    ln.toUpperCase().split('').forEach(e => {
+        if (char === e) {
+            char = e;
+            safe += 1;
+            flag = (safe === 4);
+            return;
+        } else {
+            char = e;
+            safe = 1;
+            return;
+        }
+    });
+    return flag;
+}
