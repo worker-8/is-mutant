3d6f9b959a2ed6904204b76595203339980d248f
Norman Th. Glaves <ntglaves@falabella.cl>
2019-10-18 11:27:11 -0300

    remove grid convert

--- src/logic/__tests__/col_validator.test.js
+++ src/logic/__tests__/col_validator.test.js
@@ -1,5 +1,4 @@
 const colValidator = require('../col_validator.js');
-const gridConvert = require('../grid_convert');
 
 /*
 'ATGCGA'
@@ -11,25 +10,25 @@ const gridConvert = require('../grid_convert');
 */
 
 test('find mutant DNA', () => {
-    const origin = gridConvert([
+    const origin = [
         'ATGCGA',
         'CAGTGC',
         'TTATGT',
         'TGAACG',
         'TCCCTA',
-        'TCACTG']);
+        'TCACTG'];
     const expected = 1;
     expect(colValidator(origin)).toBe(expected);
 });
 
 test('find mutant DNA not there', () => {
-    const origin = gridConvert([
+    const origin = [
         "ATGCGA",
         "CAGTGC",
         "ATATGT",
         "TGAACG",
         "TCCCTA",
-        "TCACTG"]);
+        "TCACTG"];
     const expected = 0;
     expect(colValidator(origin)).toBe(expected);
 });
