f34f9c5d40a7bdc7c8aff7fe33489e9e1b56fb10
Norman Th. Glaves <ntglaves@falabella.cl>
2019-10-18 11:56:35 -0300

    oblique validator

---
+++ src/logic/oblique_validator.js
@@ -0,0 +1,10 @@
+const isMutant = require('./is_mutant');
+module.exports = (data) => {
+    let lr = "";
+    let rl = "";
+    for (let x = 0; x <= data.length - 1; x++) {
+        lr += data[x][x];
+        rl += data[x][data.length - x - 1];
+    }
+    return isMutant(lr) + isMutant(rl);
+}
