0de290222e3f903b99b67fb783b093b46483acd6
Norman Th. Glaves <ntglaves@falabella.cl>
2019-10-21 15:57:33 -0300

    update code

--- src/logic/is_mutant.js
+++ src/logic/is_mutant.js
@@ -4,26 +4,5 @@
  * @return boolean 
  */
 module.exports = (ln) => {
-    let flag = false;
-    let safe = 0;
-    let char = '';
-
-    ln.toUpperCase().split('').forEach(e => {
-        if (char === e) {
-            char = e;
-            safe += 1;
-            
-            if (safe === 4) {
-                // console.log(ln);
-                flag = true;
-            }
-            
-            return;
-        } else {
-            char = e;
-            safe = 1;
-            return;
-        }
-    });
-    return flag;
+    return /(A{4}|C{4}|G{4}|T{4})/gmi.test(ln);
 };
