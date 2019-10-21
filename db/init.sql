f1c9eb161aa8aacba1314831d2829804fba9da6d
Norman Th. Glaves <ntglaves@falabella.cl>
2019-10-21 15:58:16 -0300

    update SQL

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,10 @@
-CREATE TABLE `registry` (
+CREATE DATABASE IF NOT EXISTS mutant_log;
+
+USE mutant_log;
+
+CREATE TABLE IF NOT EXISTS `registry` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `dna` longtext NOT NULL,
   `is_mutant` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
