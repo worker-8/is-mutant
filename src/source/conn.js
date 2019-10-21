ae9ff4f9c16ba1a993739797c01eecb0bfb139f7
Norman Th. Glaves <ntglaves@falabella.cl>
2019-10-21 15:58:01 -0300

    use env vars

--- src/source/conn.js
+++ src/source/conn.js
@@ -1,16 +1,10 @@
 let mysql = require('mysql');
 
-/**
- * host: localhost // process.env.DB_HOST
- * user: root // process.env.DB_NAME
- * password : luckystrike // process.env.DB_PASSWORD
- * database : mutant_log // process.env.DB_DATABASE
- */
-
 module.exports = mysql.createPool({
     connectionLimit: 5,
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'luckystrike',
-    database: 'mutant_log'
+    host: process.env.DB_HOST,
+    user: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
+
